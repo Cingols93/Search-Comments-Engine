48dc3a2f15f65ec22956d9661ce765dd88260913
Andrea Cingolani <cingolani.andrea@hotmail.it>
2020-12-13 16:17:02 +0100

    fix

---
+++ src/app/components/comment-list/comment-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { Comment } from '../../models/comment';
+
+@Component({
+  selector: 'app-comment-list',
+  template: `
+    <div *ngIf="comments && comments.length > 0">
+      <div class="mb-3">
+        <app-comment-list-item
+          *ngFor="let comment of comments | paginate: { itemsPerPage: 20 ,currentPage:p };"
+          [comment]="comment"
+        ></app-comment-list-item>
+      </div>
+      <pagination-controls *ngIf="comments.length > 20" (pageChange) = "p = $event" class="text-center" ></pagination-controls>
+    </div>
+
+    <h2 *ngIf="comments && comments.length == 0" class="text-center font-weight-bold">
+      No results
+    </h2>
+
+    `
+})
+export class CommentListComponent{
+  @Input() comments: Comment[];
+  p: number = 1;
+  constructor() { }
+}
